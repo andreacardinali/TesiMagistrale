f4b9e2d63368457db8e780625305e627154182b6
Andrea Cardinali <andreacardinali@gmail.com>
2023-05-23 11:26:11 +0200

    added num_classes parameter to CNN1

--- cnn.py
+++ cnn.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 
 class CNN1(nn.Module):
     #  https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
-    def __init__(self):
+    def __init__(self, num_classes):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -24,7 +24,7 @@ class CNN1(nn.Module):
 
 class CNN2(nn.Module):
     # https://github.com/cybertronai/pytorch-sso/blob/master/examples/distributed/classification/models/lenet.py
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
